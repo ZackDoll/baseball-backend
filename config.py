8331f1980d1304b9eaa32ef2d9b9db9d5db100e7
Zachary Doll <zack.j.doll@gmail.com>
2025-10-02 12:25:43 -0700

    added frontend link to CORS origin list

--- config.py
+++ config.py
@@ -4,10 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, origins=[
+    "https://frontend-baseball.onrender.com",  
+    "http://localhost:5173"
+])
 #Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pitch_data.db'
 #Disable track modifications to save resources
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
